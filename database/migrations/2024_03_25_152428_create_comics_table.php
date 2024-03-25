f85d08639b3e7a2181bb4f5916909832e9c0a95f
FrancescoCesari97 <freedus97@gmail.com>
2024-03-25 16:42:12 +0100

    aggiunto tramite migrate db comics

--- database/migrations/2024_03_25_152428_create_comics_table.php
+++ database/migrations/2024_03_25_152428_create_comics_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,10 +14,17 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type', 50);
+
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
