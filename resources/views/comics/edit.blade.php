6f92705ab1582b1f2cca98bef26511e7b51f8853
FrancescoCesari97 <freedus97@gmail.com>
2024-03-26 16:50:55 +0100

    aggiunta la pagina dell'edit partendo da quella del create e modificando la rotta e aggiungendo il medoto PATCH

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('title', 'Modifica Fumetto' . $comic->title)
+
+@section('main-content')
+    <section>
+        <div class="container py-4">
+            <h1 class="text-center">Modifica Fumetto {{ $comic->title }}</h1>
+
+            <form action="{{ route('comics.update') }}" class="py-5 row g-5" method="POST">
+                @csrf
+                @method('PATCH');
+
+                <div class="col-4">
+                    <label class="form-label" for="title">Titolo</label>
+                    <input class="form-control" type="text" id="title" name="title" required>
+                </div>
+
+                <div class="col-4">
+                    <label class="form-label" for="sale_date">data</label>
+                    <input class="form-control" type="date" id="sale_date" name="sale_date" required>
+                </div>
+
+                <div class="col-4">
+                    <label class="form-label" for="price">prezzo</label>
+                    <input class="form-control" type="text" id="price" name="price" required>
+                </div>
+
+                <div class="col-4">
+                    <label class="form-label" for="thumb">Url Immagine</label>
+                    <input class="form-control" type="url" id="thumb" name="thumb">
+                </div>
+
+
+                <div class="col-4">
+                    <label class="form-label" for="type">Tipo</label>
+                    <select class="form-control" id="type" name="type" required>
+                        <option value="comic book">comic book</option>
+                        <option value="graphic novel">graphic novel</option>
+
+                    </select>
+                </div>
+
+                <div class="col-4">
+                    <label class="form-label" for="series">Series</label>
+                    <input class="form-control" type="text" id="series" name="series" required>
+                </div>
+
+                <div class="col-12">
+                    <label class="form-label" for="description">Descrizione</label>
+                    <textarea class="form-control" name="description" id="description" rows="5"></textarea>
+                </div>
+
+
+                <div class="col-3">
+
+                    <button class="btn btn-success">crea nuovo</button>
+                </div>
+
+            </form>
+
+        </div>
+    </section>
+@endsection
